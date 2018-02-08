14b4e3f8f4046b48dfbb918f766f6c75d747b95a
angelusreseda <34175338+angelusreseda@users.noreply.github.com>
2018-02-08 12:38:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# currenthr
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CurrenthrController.js
@@ -0,0 +1,11 @@
+/**
+ * CurrenthrController
+ *
+ * @description :: Server-side logic for managing currenthrs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
